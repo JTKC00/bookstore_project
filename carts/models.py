b027950616f145a92c6e3f6d7b336535525d8cec
JamesTong <kachuntong01@gmail.com>
2025-06-21 19:26:34 +0800

    bug fix duplicate items of carts

--- carts/models.py
+++ carts/models.py
@@ -7,16 +7,12 @@ from books.models import Book
 class ShopCart(models.Model):
     datetime = models.DateTimeField(default=datetime.now, blank=True)
     userId = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True)
-    userId = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True)
+
     
 
 class CartItem(models.Model):
     bookId = models.ForeignKey(Book, on_delete=models.DO_NOTHING)
     shopCartId = models.ForeignKey(ShopCart, on_delete=models.DO_NOTHING)
-    bookId = models.ForeignKey(Book, on_delete=models.DO_NOTHING)
-    shopCartId = models.ForeignKey(ShopCart, on_delete=models.DO_NOTHING)
     quantity = models.IntegerField()
-    sub_total = models.DecimalField(max_digits=10, decimal_places=2)
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     sub_total = models.DecimalField(max_digits=10, decimal_places=2)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
