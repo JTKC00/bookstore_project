675cdfe16ebd5f12e6377f023696d6ffd310a2f7
JamesTong <kachuntong01@gmail.com>
2025-06-23 15:50:28 +0800

    revised accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
 class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name',)
+    list_per_page = 20
+    inlines = [ProfileInline]
+
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
